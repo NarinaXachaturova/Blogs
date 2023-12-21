ecd3419e68a36427aff4aa91822a9c18015b926a
narinka <narinkaxachaturova9@gmail.com>
2023-12-21 17:52:12 +0400

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#1A1A1F",
+        purple: "#5D37F3",
+        red: "#EA1919",
+        green: "#14D81C",
+        white: "#fff",
+      },
+    },
   },
   plugins: [],
 };
